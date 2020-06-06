793f9bfa41c262c3005467e5cdb306324ae0ecff
hudimujiono <hudimujiono@gmail.com>
2020-06-06 18:41:59 +0700

    create table iuran

---
+++ app/Http/Controllers/IuranController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Iuran;
+use Illuminate\Http\Request;
+
+class IuranController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Iuran  $iuran
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Iuran $iuran)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Iuran  $iuran
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Iuran $iuran)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Iuran  $iuran
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Iuran $iuran)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Iuran  $iuran
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Iuran $iuran)
+    {
+        //
+    }
+}
